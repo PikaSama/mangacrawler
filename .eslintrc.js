9039a56c5e3c81ae48f1631ca7c2a94d4474374e
PikaSama <pikasama@qq.com>
2021-01-23 16:18:52 +0800

    prettier format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-    extends: [
-        'alloy',
-        'alloy/typescript',
-    ],
+    extends: ['alloy', 'alloy/typescript'],
     env: {
         browser: true,
         node: true,
